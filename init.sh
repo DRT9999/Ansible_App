8c8917c4b49dda04d722c6dde4b87d8accfc5c82
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-29 10:08:37 +0530

    3

--- init.sh
+++ init.sh
@@ -1,1 +1,9 @@
+if [ "$pipeline" == "true" ] ;then 
+    exit 
+fi 
+
 ansible-playbook -i inv-${env} -e ansible_user=ec2-user -e ansible_password=${ssh_pwd} -e COMP=${COMP} -e env=${env} -e pwd=${pwd} expense.yml #ExpenseApp@1
+
+
+echo "Printing Environment Variables To Detect Secrets If Any"
+env 
